587bac287fa17c179ae1816a9442c00dcb84b6af
Bruno Faria <brunofaria1322@gmail.com>
2021-03-23 17:24:22 +0000

    registo sem as cenas do rmi

--- meta1/Admin/AdminConsole.java
+++ meta1/Admin/AdminConsole.java
@@ -1,4 +1,7 @@
+import java.lang.invoke.StringConcatFactory;
 import java.rmi.registry.LocateRegistry;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 import java.util.Scanner;
 
 public class AdminConsole {
@@ -6,26 +9,32 @@ public class AdminConsole {
         
         /*
         try {
-			Bla bla = (Bla) LocateRegistry.getRegistry(7000).lookup("benfica");
+			Bla bla = (Bla) LocateRegistry.getRegistry(1099).lookup("central");
 		} catch (Exception e) {
 			System.out.println("Exception in main: " + e);
 			e.printStackTrace();
 		}
         */
-        int option = 0;
+        System.out.println("Welcome to Admin Console");
+
+        int option;
         Scanner in = new Scanner(System.in);
         do{
-            System.out.println("Welcome to Admin Console\n1 - Regist person\n2 - Create election\n3 - Manage election\n0 - Quit");
+            System.out.print("\n1 - Regist person\n2 - Create election\n3 - Manage election\n0 - Quit\noption: ");
 
             option = in.nextInt();
             in.nextLine();
             
             switch (option){
                 case 0:
-                    System.out.println("Option 0!");
+                    System.out.println("Bye!");
                     break;
                 case 1:
-                    System.out.println("Option 1!");
+                    if (registerPerson(in)){
+                        System.out.println("Person successfully registered!");
+                    } else {
+                        System.out.println("Registry was aborted!");
+                    }
                     break;
                 case 2:
                     System.out.println("Option 2!");
@@ -39,5 +48,117 @@ public class AdminConsole {
             }
         }while (option != 0);
         in.close();
+    }
+
+    private static boolean registerPerson(Scanner in){
+        int type, ndep;
+        boolean isValid = false;
+        do{
+            System.out.print("\n1 - Regist student\n2 - Regist teacher\n3 - Regist employee\n0 - Abort\noption: ");
+
+            type = in.nextInt();
+            in.nextLine();
+            
+            switch (type){
+                case 0:
+                    return false;
+                case 1:
+                case 2:
+                case 3:
+                    isValid = true;
+                    break;
+                default:
+                    System.out.println("Wrong option!");
+                    break;
+            }
+        }while (!isValid);
+
+        //TODO: escolher departamento da BD
+
+        isValid = false;
+        System.out.println("bla bla bla... selecionar o departamento a que pertence");
+        do{
+            System.out.print("0 - Abort\noption: ");
+
+            ndep = in.nextInt();
+            in.nextLine();
+            
+            if (ndep == 0){
+                return false;
+            }
+
+            else if (ndep< 0 || ndep > 19){     //TODO: dep > num de departamentos
+                System.out.println("Wrong option!");
+                break;
+            }
+            else{
+                isValid=true;
+            }
+        }while (!isValid);
+
+        System.out.print("\nName: ");
+        String name = in.nextLine();
+
+        System.out.print("\nAddress: ");
+        String address = in.nextLine();
+
+        System.out.print("\nPhone number: ");
+        String phone_number = in.nextLine();
+
+        System.out.print("\nCC number: ");
+        String cc_number = in.nextLine();
+
+        isValid=false;
+        Date cc_expiration_date = new Date();
+        do{
+            System.out.print("\nCC expiration date (dd/mm/yyyy): ");
+            String aux = in.nextLine();
+            try{
+                cc_expiration_date = new SimpleDateFormat("dd/MM/yyyy").parse(aux);
+                isValid = true;
+            }catch(Exception e){
+                System.out.print("Invalid date! Date should be in format (dd/mm/yyyy).");
+            }
+
+        }while(!isValid);
+
+        System.out.print("\nUsername: ");
+        String username = in.nextLine();
+
+        isValid = false;
+        String password;
+        do{ 
+            System.out.print("\nPassword: ");
+            password = in.nextLine();
+            if(password.length()>=4){
+                System.out.print("Confirm password: ");
+                String pv = in.nextLine();
+                if (password.equals(pv)){
+                    isValid=true;
+                }else{
+                    System.out.print("\nPasswords dont match! Try Again.");
+                }
+            }else{
+                System.out.print("\nPassword requires a minimum of 4 characters! Try Again.");
+            }
+        }while(!isValid);
+
+
+        //TODO:contactar servidor
+
+        //DEBUG
+        System.out.println(
+            "\ntype: " + type +
+            "\nndep: " + ndep +
+            "\nname: " + name +
+            "\naddress: " + address +
+            "\nPhone no: " + phone_number +
+            "\nCC: " + cc_number +
+            "\nCC val: " + cc_expiration_date.toString() +
+            "\nusername: " + username +
+            "\npassword: " + password
+        );
+        return true;
+        
     }
 }
