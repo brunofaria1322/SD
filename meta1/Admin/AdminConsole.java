b4725e79660419fa65f7f9f625b7280a742ea8a0
Bruno Faria <brunofaria1322@gmail.com>
2021-03-24 17:17:29 +0000

    create Election()

--- meta1/Admin/AdminConsole.java
+++ meta1/Admin/AdminConsole.java
@@ -1,10 +1,12 @@
 import java.text.SimpleDateFormat;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
 import java.util.Date;
 import java.util.Scanner;
 
 public class AdminConsole {
     public static void main(String[] args) {
-        
+
         /*
         try {
 			Bla bla = (Bla) LocateRegistry.getRegistry(1099).lookup("central");
@@ -35,7 +37,11 @@ public class AdminConsole {
                     }
                     break;
                 case 2:
-                    System.out.println("Option 2!");
+                    if (createElection(in)){
+                        System.out.println("Election successfully created!");
+                    } else {
+                        System.out.println("Election creation was aborted!");
+                    }
                     break;
                 case 3:
                     System.out.println("Option 3!");
@@ -74,7 +80,7 @@ public class AdminConsole {
         //TODO: escolher departamento da BD
 
         isValid = false;
-        System.out.println("bla bla bla... selecionar o departamento a que pertence");
+        System.out.println("\nbla bla bla... selecionar o departamento a que pertence");
         do{
             System.out.print("0 - Abort\noption: ");
 
@@ -86,7 +92,7 @@ public class AdminConsole {
             }
 
             else if (ndep< 0 || ndep > 19){     //TODO: dep > num de departamentos
-                System.out.println("Wrong option!");
+                System.out.println("Wrong option! Try Again...");
                 break;
             }
             else{
@@ -115,7 +121,7 @@ public class AdminConsole {
                 cc_expiration_date = new SimpleDateFormat("dd/MM/yyyy").parse(aux);
                 isValid = true;
             }catch(Exception e){
-                System.out.print("Invalid date! Date should be in format (dd/mm/yyyy).");
+                System.out.print("Invalid date! Date should be in format (dd/mm/yyyy). Try Again...");
             }
 
         }while(!isValid);
@@ -134,10 +140,10 @@ public class AdminConsole {
                 if (password.equals(pv)){
                     isValid=true;
                 }else{
-                    System.out.print("\nPasswords dont match! Try Again.");
+                    System.out.print("\nPasswords dont match! Try Again...");
                 }
             }else{
-                System.out.print("\nPassword requires a minimum of 4 characters! Try Again.");
+                System.out.print("\nPassword requires a minimum of 4 characters! Try Again...");
             }
         }while(!isValid);
 
@@ -155,6 +161,113 @@ public class AdminConsole {
             "\nCC val: " + cc_expiration_date.toString() +
             "\nusername: " + username +
             "\npassword: " + password
+        );
+        return true;
+        
+    }
+
+    private static boolean createElection(Scanner in){
+        int voters, ndep;
+        boolean isValid = false;
+        do{
+            System.out.print("\nWho are the voters?\n1 - Students\n2 - Teachers\n3 - Employees\n0 - Abort\noption: ");
+
+            voters = in.nextInt();
+            in.nextLine();
+            
+            switch (voters){
+                case 0:
+                    return false;
+                case 1:
+                case 2:
+                case 3:
+                    isValid = true;
+                    break;
+                default:
+                    System.out.println("Wrong option! Try Again...");
+                    break;
+            }
+        }while (!isValid);
+
+        //TODO: escolher departamento da BD
+
+        isValid = false;
+        System.out.println("\nbla bla bla... selecionar o departamento a que pertence");
+        do{
+            System.out.print("0 - Abort\noption: ");
+
+            ndep = in.nextInt();
+            in.nextLine();
+            
+            if (ndep == 0){
+                return false;
+            }
+
+            else if (ndep< 0 || ndep > 19){     //TODO: dep > num de departamentos
+                System.out.println("Wrong option! Try Again...");
+                break;
+            }
+            else{
+                isValid=true;
+            }
+        }while (!isValid);
+
+        System.out.print("\nElection title: ");
+        String title = in.nextLine();
+
+        System.out.print("\nElection description: ");
+        String description = in.nextLine();
+
+        isValid=false;
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd/MM/yyyy HH:mm");
+        LocalDateTime start_time = LocalDateTime.now();
+        do{
+            System.out.print("\nElection start (dd/MM/yyyy HH:mm): ");
+            String aux = in.nextLine();
+            try{
+                start_time = LocalDateTime.parse(aux, formatter);
+                if(start_time.isAfter(LocalDateTime.now())){
+                    isValid = true;
+                }else{
+                    System.out.print("Start date and time already passed. Try Again...");
+                }
+            }catch(Exception e){
+                System.out.print("Invalid date and time! Try Again...");
+            }
+
+        }while(!isValid);
+
+        isValid=false;
+        LocalDateTime end_time = LocalDateTime.now();
+        do{
+            System.out.print("\nElection end (dd/MM/yyyy HH:mm): ");
+            String aux = in.nextLine();
+            try{
+                end_time = LocalDateTime.parse(aux, formatter);
+
+                if(end_time.isAfter(start_time)){
+                    isValid = true;
+                }else{
+                    System.out.print("End needs to be after start time! Try Again...");
+                }
+
+            }catch(Exception e){
+                System.out.print("Invalid date and time! Try Again...");
+            }
+
+        }while(!isValid);
+
+
+        //TODO:contactar servidor
+
+        //DEBUG
+        System.out.println(
+            "\ntype: " + voters +
+            "\nndep: " + ndep +
+            "\ntitle: " + title +
+            "\ndescription: " + description +
+            "\nstart: " + start_time.toString() +
+            "\nend: " + end_time.toString()
         );
         return true;
         
