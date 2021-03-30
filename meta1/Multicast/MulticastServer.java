ad5df953e1182ffaf6f2fbc7bc867727591806bc
Bruno Faria <brunofaria1322@gmail.com>
2021-03-30 02:18:01 +0100

    atribuição de ids aos terminais

--- meta1/Multicast/MulticastServer.java
+++ meta1/Multicast/MulticastServer.java
@@ -1,4 +1,5 @@
 import java.net.MulticastSocket;
+import java.util.HashMap;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.io.IOException;
@@ -6,7 +7,7 @@ import java.io.IOException;
 public class MulticastServer extends Thread {
     private String MULTICAST_ADDRESS = "224.3.2.1";
     private int PORT = 4321;
-    private long SLEEP_TIME = 5000;
+    private String DEP = "NE";
 
     public static void main(String[] args) {
         MulticastServer server = new MulticastServer();
@@ -14,29 +15,109 @@ public class MulticastServer extends Thread {
     }
 
     public MulticastServer() {
-        super("Server " + (long) (Math.random() * 1000));
+        //TODO: ler config.txt
     }
 
     public void run() {
         MulticastSocket socket = null;
-        long counter = 0;
-        System.out.println(this.getName() + " running...");
+        int ids = 0;
+        System.out.println(DEP + " Polling station running...");
         try {
-            socket = new MulticastSocket();  // create socket without binding it (only for sending)
+            InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+
+            PollingStationInterface it = new PollingStationInterface(group, PORT);
+
+            socket = new MulticastSocket(PORT);  // create socket and bind it
+            socket.joinGroup(group);
+
+            byte[] buffer;
+            DatagramPacket packet;
+            String message;
+            
+            HashMap<String,String> hash_map;
+
             while (true) {
-                String message = this.getName() + " packet " + counter++;
-                byte[] buffer = message.getBytes();
+                buffer = new byte[256];
+                packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
 
-                InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length, group, PORT);
-                socket.send(packet);
+                //DEBUG
+                System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + " with message:");
+                message = new String(packet.getData(), 0, packet.getLength());
+                System.out.println(message);
 
-                try { sleep((long) (Math.random() * SLEEP_TIME)); } catch (InterruptedException e) { }
+                hash_map = it.packetToHashMap(packet);
+                
+                switch (hash_map.get("type")) {
+                    case "identification":
+                        if(hash_map.get("id") == null){
+                            it.sendMessage("type | identification; id | " + ids++);
+                        }
+                        break;
+                
+                    default:
+                        break;
+                }
             }
         } catch (IOException e) {
             e.printStackTrace();
         } finally {
             socket.close();
         }
+    }
+}
+
+class PollingStationInterface extends Thread{
+    private MulticastSocket socket;
+    private InetAddress group;
+    private int PORT;
+
+    public PollingStationInterface(InetAddress group, int PORT){
+        this.group = group;
+        this.PORT = PORT;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            this.socket = new MulticastSocket();  // create socket without binding it (only for sending)
+
+            while(true){
+                try { sleep((long) 10000); } catch (InterruptedException e) { }
+            }
+
+            //TODO: AQUI
+                
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+    }
+
+    public void sendMessage( String message ){
+        try {
+            byte[] buffer = message.getBytes();
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, this.group, this.PORT);
+            this.socket.send(packet);
+                
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    public HashMap<String, String> packetToHashMap(DatagramPacket packet) {
+        String message = new String(packet.getData(), 0, packet.getLength());
+        HashMap<String, String> hash_map = new HashMap<String, String>();
+            
+        for(String aux : message.split(";")){
+            String key_value[] = aux.split("\\|");
+            
+            hash_map.put(key_value[0].trim(), key_value[1].trim());
+        }
+        
+        //System.out.println("String:\n\t" + message + "\nto HashMap:\n\t" + hash_map);
+        return hash_map;
     }
 }
--- meta1/Voting/VotingTerminal.java
+++ meta1/Voting/VotingTerminal.java
@@ -1,4 +1,5 @@
 import java.net.MulticastSocket;
+import java.util.HashMap;
 import java.net.DatagramPacket;
 import java.net.InetAddress;
 import java.io.IOException;
@@ -6,6 +7,7 @@ import java.io.IOException;
 public class VotingTerminal extends Thread {
     private String MULTICAST_ADDRESS = "224.3.2.1";
     private int PORT = 4321;
+    private int id = -1;
 
     public static void main(String[] args) {
         VotingTerminal client = new VotingTerminal();
@@ -15,16 +17,41 @@ public class VotingTerminal extends Thread {
     public void run() {
         MulticastSocket socket = null;
         try {
-            socket = new MulticastSocket(PORT);  // create socket and bind it
             InetAddress group = InetAddress.getByName(MULTICAST_ADDRESS);
+            VotingInterface it = new VotingInterface(group, PORT);          //TODO: apenas criar quando desbloqueado
+
+            socket = new MulticastSocket(PORT);  // create socket and bind it
             socket.joinGroup(group);
+
+            String message = "type | identification";
+            it.sendMessage(message);
+
+            byte[] buffer = new byte[256];
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+            socket.receive(packet);
+
+            HashMap<String,String> hash_map;
+            
+            while (this.id == -1) {
+                buffer = new byte[256];
+                packet = new DatagramPacket(buffer, buffer.length);
+                socket.receive(packet);
+
+                hash_map = it.packetToHashMap(packet);
+                if(hash_map.get("type").equals("identification") && hash_map.get("id") != null){
+                    this.id = Integer.parseInt(hash_map.get("id"));
+                }
+            }
+
+            System.out.println("I Have Id = "+ this.id);
+
             while (true) {
-                byte[] buffer = new byte[256];
-                DatagramPacket packet = new DatagramPacket(buffer, buffer.length);
+                buffer = new byte[256];
+                packet = new DatagramPacket(buffer, buffer.length);
                 socket.receive(packet);
 
                 System.out.println("Received packet from " + packet.getAddress().getHostAddress() + ":" + packet.getPort() + " with message:");
-                String message = new String(packet.getData(), 0, packet.getLength());
+                message = new String(packet.getData(), 0, packet.getLength());
                 System.out.println(message);
             }
         } catch (IOException e) {
@@ -32,5 +59,66 @@ public class VotingTerminal extends Thread {
         } finally {
             socket.close();
         }
+    }
+}
+
+class VotingInterface extends Thread{
+    private MulticastSocket socket;
+    private InetAddress group;
+    private int PORT;
+
+    public VotingInterface(InetAddress group, int PORT){
+        this.group = group;
+        this.PORT = PORT;
+        this.start();
+    }
+
+    public void run() {
+        try {
+            this.socket = new MulticastSocket();  // create socket without binding it (only for sending)
+
+            while(true){
+                try { sleep((long) 10000); } catch (InterruptedException e) { }
+            }
+
+            //TODO:
+
+                
+        } catch (IOException e) {
+            e.printStackTrace();
+        } finally {
+            socket.close();
+        }
+    }
+
+    public void unlock(){
+        //TODO: This
+    }
+
+    public void sendMessage( String message ){
+        try {
+            byte[] buffer = message.getBytes();
+
+            DatagramPacket packet = new DatagramPacket(buffer, buffer.length, this.group, this.PORT);
+            this.socket.send(packet);
+                
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    
+    
+    public HashMap<String, String> packetToHashMap(DatagramPacket packet) {
+        String message = new String(packet.getData(), 0, packet.getLength());
+        HashMap<String, String> hash_map = new HashMap<String, String>();
+            
+        for(String aux : message.split(";")){
+            String key_value[] = aux.split("\\|");
+            
+            hash_map.put(key_value[0].trim(), key_value[1].trim());
+        }
+        
+        //System.out.println("String:\n\t" + message + "\nto HashMap:\n\t" + hash_map);
+        return hash_map;
     }
 }
