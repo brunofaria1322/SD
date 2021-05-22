f2a96b7b439a73eab44ad5aab2e90e60b0057671
Bruno Faria <brunofaria1322@gmail.com>
2021-05-22 22:11:00 +0100

    para continuar....

--- meta2/eVoting/src/WebInterface/action/VoterIndexAction.java
+++ meta2/eVoting/src/WebInterface/action/VoterIndexAction.java
@@ -1,9 +1,11 @@
 
 package WebInterface.action;
 
+import WebInterface.model.WebServer;
 import com.opensymphony.xwork2.ActionSupport;
 import org.apache.struts2.interceptor.SessionAware;
 
+import java.io.IOException;
 import java.io.Serial;
 import java.util.HashMap;
 import java.util.Map;
@@ -15,38 +17,14 @@ public class VoterIndexAction extends ActionSupport implements SessionAware {
 
 	private HashMap<Integer, HashMap<String,String>> electionsList;
 
-	public VoterIndexAction(){
+	public VoterIndexAction() throws IOException {
 
 		//elections
-		electionsList = new HashMap<>();
-		//TODO: get lista de eleições que user pode ou pôde votar
-		HashMap<String, String> temp = new HashMap<>();
-		temp.put("titulo", "Por começar");
-		temp.put("descricao", "esta eleiçai ainda nem começou....");
-		temp.put("inicio", "2021-06-21 17:15:00");
-		temp.put("fim", "2021-06-26 21:30:00");
-		temp.put("departamentos", ";4;1;");
-		temp.put("mesas", ";4;");
-
-		electionsList.put(1, temp);
-
-		temp = new HashMap<>();
-		temp.put("titulo", "Ativa");
-		temp.put("descricao", "esta eleição está ativa e a decorrer...");
-		temp.put("inicio", "2021-05-20 17:15:00");
-		temp.put("fim", "2021-06-23 21:30:00");
-		temp.put("departamentos", ";4;1;");
-		temp.put("mesas", ";4;");
-		electionsList.put(3, temp);
-
-		temp = new HashMap<>();
-		temp.put("titulo", "Terminada");
-		temp.put("descricao", "esta eleição está terminada...");
-		temp.put("inicio", "2021-04-20 17:15:00");
-		temp.put("fim", "2021-04-23 21:30:00");
-		temp.put("departamentos", ";4;1;");
-		temp.put("mesas", ";4;");
-		electionsList.put(2, temp);
+		WebServer wb = new WebServer();
+		wb.readConfig();
+		wb.connect();
+		electionsList = wb.getElections((String) session.get("username"), null);
+		//TODO: falta completar aaaaaquiiiii
 	}
 
 
