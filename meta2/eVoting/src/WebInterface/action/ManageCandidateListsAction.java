f0599a0b983641f5216983fda746715b6e3a0b28
Bruno Faria <brunofaria1322@gmail.com>
2021-05-22 21:36:01 +0100

    cagar no remove

--- meta2/eVoting/src/WebInterface/action/ManageCandidateListsAction.java
+++ meta2/eVoting/src/WebInterface/action/ManageCandidateListsAction.java
@@ -47,12 +47,15 @@ public class ManageCandidateListsAction extends ActionSupport implements Session
 		//results = {list_id, ListName, }
 		candidates = new HashMap<>();
 		for(Integer key : lists.keySet()){
-			results.add(new String[]{key.toString(),lists.get(key).left});
-			List<String[]> temp = new ArrayList<>();
-			for(database.Pair<String, String> candidato: lists.get(key).right){
-				temp.add(new String[] {candidato.left,candidato.right});
+			if(lists.get(key).left.compareTo("votos nulos") != 0 && lists.get(key).left.compareTo("votos em branco") != 0 ) {
+
+				results.add(new String[]{key.toString(), lists.get(key).left});
+				List<String[]> temp = new ArrayList<>();
+				for (database.Pair<String, String> candidato : lists.get(key).right) {
+					temp.add(new String[]{candidato.left, candidato.right});
+				}
+				candidates.put(key.toString(), temp);
 			}
-			candidates.put(key.toString(),temp);
 		}
 
 		return SUCCESS;
@@ -65,56 +68,56 @@ public class ManageCandidateListsAction extends ActionSupport implements Session
 		}
 		else if(res == -3){
 			session.put("error","There's already a list with that name.");
-			return "none";
+			return NONE;
 		}
 		else {
 			session.put("error","List could not be added.");
-			return "none";
+			return NONE;
 		}
 	}
 
 	public String removeCandidateList() throws RemoteException {
-		Integer[] list = {new Integer(listId)};
+		Integer[] list = {Integer.valueOf(listId)};
 		int res = getWebServer().editElection((Integer)session.get("electionId"), true, null, null, null, null, null, null,list );
 		if(res >0){
 			return SUCCESS;
 		}
 		else {
 			session.put("error","List could not be removed.");
-			return "none";
+			return NONE;
 		}
 	}
 
 	public String addCandidate() throws RemoteException {
 		ArrayList<database.Pair<String, String>> users = new ArrayList<>();
-		users.add(new database.Pair<String,String>(candidateId,candidateName));
+		users.add(new database.Pair<>(candidateId,candidateName));
 		int res = getWebServer().createOrEditList((Integer) session.get("electionId"),listName,users,false);
 		if(res >0){
 			return SUCCESS;
 		}
 		else if(res == -2){
 			session.put("error","That user cannot be a member of that list.");
-			return "none";
+			return NONE;
 		}
 		else {
 			session.put("error","User could not be added.");
-			return "none";
+			return NONE;
 		}
 	}
 
 	public String removeCandidate() throws RemoteException {
 
-
 		ArrayList<database.Pair<String, String>> users = new ArrayList<>();
-		users.add(new database.Pair<String,String>(candidateId,candidateName));
+		users.add(new database.Pair<>(candidateId, candidateName));
 		int res = getWebServer().createOrEditList((Integer) session.get("electionId"),listName,users,true);
 		if(res >0){
 			return SUCCESS;
 		}
 		else {
 			System.out.println(res);
+			System.out.println(candidateId + " id of candidate" + candidateName + "\ton elec with id = " + session.get("electionId"));
 			session.put("error","User could not be removed.");
-			return "none";
+			return NONE;
 		}
 	}
 	public WebServer getWebServer() throws RemoteException {
